e4dbf7fc5b0d94a88e2e33a6b75861c276adb37d
Cronoscar <eucedao662@gmail.com>
2025-11-08 09:30:24 -0600

    Backend_SpottyApp

--- src/index.js
+++ src/index.js
@@ -1,11 +1,22 @@
 import express from "express";
 import dotenv from "dotenv";
+import { getConnection ,sql} from "../utils/db.js";
 dotenv.config();
 const app = express();
 const port = process.env.PORT ;
 app.get("/", (req, res) => {
   res.send("Hello World!");
+  
 });
 app.listen(port, () => {
   console.log(`server running on port ${port}`);
 }); 
+
+try {
+  const pool = await getConnection();
+  const result = await pool.request().query("SELECT  a.ID, a.Nombre,a.Apellido,b.Vehiculo FROM users.tblPersonas as a, users.tblClientes as b WHERE a.ID=b.ID_CLIENTE");
+  console.log(result.recordset);
+} catch (error) {
+  console.error("Error executing query:", error);
+}
+export default app;
--- utils/db.js
+++ utils/db.js
@@ -3,29 +3,30 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-// Configuración de la conexión a SQL Server
+// Configuración de conexión a SQL Server
 const dbConfig = {
-  user: process.env.DB_USER || "industria",
-  password: process.env.DB_PASS || "industria123",
-  server: process.env.DB_SERVER || "localhost", // Cambia si usas otro host o instancia
-  database: process.env.DB_NAME || "prueba",
+    user: process.env.DB_USER ,
+  password: process.env.DB_PASS  ,
+  server: process.env.DB_SERVER , // Cambia si usas una instancia: "localhost\\SQLEXPRESS"
+  database: process.env.DB_NAME  ,
   options: {
-    encrypt: false, // Si usas Azure ponlo en true
-    trustServerCertificate: true, // Necesario para entornos locales
+    encrypt: false, // true si es Azure
+    trustServerCertificate: true, // necesario para conexiones locales
+    instanceName: 'SQLEXPRESS', // Dejar vacío si no usas una instancia
   },
 };
 
 // Crear un pool de conexión global
 let pool;
 
-export const getConnection = async () => {
+export  const getConnection = async () => {
   try {
     if (pool) {
-      // Reusar el pool existente si ya está conectado
+      // Reusar conexión existente
       return pool;
     }
     pool = await sql.connect(dbConfig);
-    console.log("✅ Conexión exitosa a SQL Server (base de datos 'prueba')");
+    console.log("✅ Conexión exitosa a SQL Server (base de datos 'ProyectoSpotty')");
     return pool;
   } catch (error) {
     console.error("❌ Error al conectar con SQL Server:", error.message);
